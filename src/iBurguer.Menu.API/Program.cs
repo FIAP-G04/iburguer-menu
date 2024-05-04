c6201059afa195533c8b060fd7d6ed79a265a50d
matheusantonio <matheus.antonio.c@outlook.com>
2024-05-04 14:20:53 -0300

    Adicionados artefatos kubernetes

--- src/iBurguer.Menu.API/Program.cs
+++ src/iBurguer.Menu.API/Program.cs
@@ -12,7 +12,10 @@ builder.AddWebApi()
        .AddUseCases()
        .AddSerilog();
 
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
 app.UseWebApi();
+app.MapHealthChecks("/hc");
 app.Run();
