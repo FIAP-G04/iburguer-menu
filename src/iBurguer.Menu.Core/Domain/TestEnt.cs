e8d7a4848f9afd04675f1838853ac014ff01c6b5
matheusantonio <matheus.antonio.c@outlook.com>
2024-05-18 21:32:23 -0300

    tst

---
+++ src/iBurguer.Menu.Core/Domain/TestEnt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iBurguer.Menu.Core.Domain
+{
+    public class TestEnt
+    {
+        public string PrintString()
+        {
+            return "Hello, Please detect me, Sonar!";
+        }
+    }
+}
