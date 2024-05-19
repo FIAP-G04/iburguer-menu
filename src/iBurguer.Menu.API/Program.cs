13c9169739b895f14663e22bee1baaafff80eb31
LUAN CUNHA <luan.cunha@viavarejo.com.br>
2024-05-18 22:59:45 -0300

    chore: exclude program.cs

--- src/iBurguer.Menu.API/Program.cs
+++ src/iBurguer.Menu.API/Program.cs
@@ -1,7 +1,6 @@
 using iBurguer.Menu.Infrastructure.IoC;
 using iBurguer.Menu.Infrastructure.Logger;
 using iBurguer.Menu.Infrastructure.MongoDb.Extensions;
-using iBurguer.Menu.Infrastructure.Swagger;
 using iBurguer.Menu.Infrastructure.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
